d3efac26f39803b470f336b305ba58bd58944afd
Cyrus Jose <cyrusjose@Cyruss-Mac-mini.local>
2020-05-24 20:01:00 -0700

    Added prompts in the the function.

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,9 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function generatePassword(){
-
+  const numberOfChar = prompt("How many characters would you like to have?");
+  const alphaNum = "abcdefghijklmnopqrstuvwxyz1234567890";
+  const specialChar = confirm("Would you like special characters in your password?");
 };
 function writePassword() {
   // you can create a function named generatePassword that creates the password
