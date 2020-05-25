d473e4af0dd0b4684dd7b25d0a8c618fac3ced64
Cyrus Jose <cyrusjose@Cyruss-Mac-mini.local>
2020-05-24 21:22:04 -0700

    Made a conditional for password length

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,11 +3,19 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function generatePassword(){
+  // Ask the user how many characters they would like to have.
   const numberOfChar = prompt("How many characters would you like to have?");
+  // Declare a vriable for all the Alpha Numeric characters and store them. 
   const alphaNum = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", 1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
-
-  const specialCharQuestion = confirm("Would you like special characters in your password?");
+  // Declare the special character variable and store the array of special characters in the variable.
   const specialChar = ["!","@","#","$","%","^","&","*","?","~"];
+  // Conditional for minimum of 8 characters 
+  if (numberOfChar < 8 || numberOfChar > 128){
+    alert("Please make sure the length of your password is at least 8 characters and no more that 128 characters.");
+  } else{
+    // Confirm with the user whether or not they want special characters.
+  const specialCharQuestion = confirm("Would you like special characters in your password?");
+  }
 };
 function writePassword() {
   // you can create a function named generatePassword that creates the password
