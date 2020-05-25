e078e6a988aaad3bf03933eb0fc4332a9fba39f3
Cyrus Jose <cyrusjose@Cyruss-Mac-mini.local>
2020-05-24 21:12:53 -0700

    Created arrays for the alphaNum and specialChar variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,9 +4,10 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 function generatePassword(){
   const numberOfChar = prompt("How many characters would you like to have?");
-  const alphaNum = "abcdefghijklmnopqrstuvwxyz1234567890";
+  const alphaNum = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", 1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+
   const specialCharQuestion = confirm("Would you like special characters in your password?");
-  const specialChar = "!@#$%^&*?~"
+  const specialChar = ["!","@","#","$","%","^","&","*","?","~"];
 };
 function writePassword() {
   // you can create a function named generatePassword that creates the password
