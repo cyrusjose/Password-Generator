38f0214cff9d8b56ecd612bd0a1c35f2e24a4436
Cyrus Jose <cyrusjose@Cyruss-Mac-mini.local>
2020-05-25 09:11:15 -0700

    tested with and without else statement.

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,19 +2,19 @@
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function generatePassword(){
+function generatePassword() {
   // Ask the user how many characters they would like to have.
   const numberOfChar = prompt("How many characters would you like to have?");
   // Declare a vriable for all the Alpha Numeric characters and store them. 
   let alphaNum = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", 1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
   // Declare the special character variable and store the array of special characters in the variable.
-  let specialChar = ["!","@","#","$","%","^","&","*","?","~"];
+  let specialChar = ["!", "@", "#", "$", "%", "^", "&", "*", "?", "~"];
   // Conditional for minimum of 8 characters 
-  if (numberOfChar < 8 || numberOfChar > 128){
+  if (numberOfChar < 8 || numberOfChar > 128) {
     alert("Please make sure the length of your password is at least 8 characters and no more that 128 characters.");
-  } else{
-    // Confirm with the user whether or not they want special characters.
-  const specialCharQuestion = confirm("Would you like special characters in your password?");
+  } else {
+  // Confirm with the user whether or not they want special characters.
+  var specialCharQuestion = confirm("Would you like special characters in your password?");
   }
 };
 function writePassword() {
