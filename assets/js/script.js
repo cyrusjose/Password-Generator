0696b2b5fcb83846f1b1d0429cf8a71b3219527e
Cyrus Jose <cyrusjose@Cyruss-Mac-mini.local>
2020-05-25 13:02:25 -0700

    test

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,27 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
+// Declare variables
+var alphanum = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+var withSpecial = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*?()_+=-~`{}[];";
+// Ask user for password length;
+var strength = prompt("How many characters would you like in your password?");
+// Check to see if it is too short or too long
+if (strength < 8) {
+  alert("Your password would be too short.");
+  window.location.reload();
+} else if (strength > 128) {
+  alert("Your password would be too long.");
+  window.location.reload();
+} else {
+  var specialChar = confirm("Would you like special character in your password?")
+}
 // Write password to the #password input
 function generatePassword() {
-  var alphanum = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  var withSpecial = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*?"
-  var length = prompt("How many characters would you like in your password?");
-  if (length < 8){
-    alert("Your password would be too short.");
-  } else if (length > 128){
-    alert("Your password would be too long.");
-  } else {
-    confirm("Would you like special character in your password?")
+  var possiblePass = "";
+  if (specialChar === true) {
+    for (var i = 0; i <= strength; i++) {
+      possiblePass = possiblePass + withSpecial.charAt(Math.floor(Math.random() * Math.floor(withSpecial.length - 1)));
+    }
   }
 }
 function writePassword() {
