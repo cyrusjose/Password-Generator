91f1ccd8390929aee79e4fb66add122a87d82379
Cyrus Jose <cyrus.jose@outlook.com>
2020-05-27 21:11:17 -0700

    Refactor

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,11 +8,13 @@ function generatePassword() {
 var warning = alert("Please make sure you have at least 8 and no more than 128 characters.");
 var strength = prompt("How many characters would you like in your password?");
 // Check to see if it is too short or too long
-if (strength < 8) {
+if (strength < 8  && strength > 0) {
   alert("Your password would be too short.");
   window.location.reload();
 } else if (strength > 128) {
   alert("Your password would be too long.");
+  window.location.reload();
+} else if (!strength) {
   window.location.reload();
 } else {
   var specialChar = confirm("Would you like special character in your password?");
